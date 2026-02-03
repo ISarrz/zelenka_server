01b73cae706a40abd9d0b5876c30e5cbaeae61cf
Zlobin German <gezlobin@edu.hse.ru>
2026-02-04 01:44:50 +0300

    init

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "--- Начинаю обновление системы ---"
-
+export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get upgrade -y
 apt-get dist-upgrade -y
@@ -10,6 +10,16 @@ apt-get autoclean -y
 
 apt install git -y
 git clone https://github.com/ISarrz/zelenka_server.git
+cd zelenka_server
 git checkout server
+
+apt install gnupg -y
+apt install -y gnupg debconf-utils wget
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb
+echo "mysql-apt-config mysql-apt-config/select-server select mysql-8.0" | debconf-set-selections
+
+dpkg -i mysql-apt-config_0.8.29-1_all.deb
+apt update
+apt install -y mysql-server
 
 echo "--- Обновление завершено успешно! ---"
