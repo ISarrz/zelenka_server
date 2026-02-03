220dea3f722d9017d89343553665e1a973f3ecbf
Zlobin German <gezlobin@edu.hse.ru>
2026-02-04 01:23:20 +0300

    init 0.0.1

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
-apt update
-apt upgrade
+#!/bin/bash
+
+echo "--- Начинаю обновление системы ---"
+
+apt-get update -y
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get autoremove -y
+apt-get autoclean -y
+
+echo "--- Обновление завершено успешно! ---"
