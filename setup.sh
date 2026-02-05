599763146841d90b5da9b4972fa771cefd8c164d
Zlobin German <gezlobin@edu.hse.ru>
2026-02-05 04:03:55 +0300

    init

--- setup.sh
+++ setup.sh
@@ -14,20 +14,34 @@ apt -y install gnupg
 
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb
 dpkg -i mysql-apt-config_0.8.29-1_all.deb
-#gpg --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C
-#gpg --export B7B3B788A8D3785C | tee /usr/share/keyrings/mysql-archive-keyring.gpg > /dev/null
-#
-#set -euo pipefail
-#
-#FILE="/etc/apt/sources.list.d/mysql.list"
-#
-#sudo tee "$FILE" > /dev/null <<'EOF'
-#deb [signed-by=/usr/share/keyrings/mysql-archive-keyring.gpg] http://repo.mysql.com/apt/debian/ bookworm mysql-8.0
-#deb-src [signed-by=/usr/share/keyrings/mysql-archive-keyring.gpg] http://repo.mysql.com/apt/debian/ bookworm mysql-8.0
-#EOF
-#
-#apt update
-#apt install mysql-server
+gpg --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C
+gpg --export B7B3B788A8D3785C | tee /usr/share/keyrings/mysql-archive-keyring.gpg > /dev/null
+
+set -euo pipefail
+
+FILE="/etc/apt/sources.list.d/mysql.list"
+
+sudo tee "$FILE" > /dev/null <<'EOF'
+deb [signed-by=/usr/share/keyrings/mysql-archive-keyring.gpg] http://repo.mysql.com/apt/debian/ bookworm mysql-8.0
+deb-src [signed-by=/usr/share/keyrings/mysql-archive-keyring.gpg] http://repo.mysql.com/apt/debian/ bookworm mysql-8.0
+EOF
+
+apt update
+!/usr/bin/env bash
+set -euo pipefail
+
+read -rsp "Введите пароль для MySQL root: " MYSQL_ROOT_PASSWORD
+echo
+
+sudo debconf-set-selections <<EOF
+mysql-community-server mysql-community-server/root-pass password $MYSQL_ROOT_PASSWORD
+mysql-community-server mysql-community-server/re-root-pass password $MYSQL_ROOT_PASSWORD
+EOF
+
+
+sudo DEBIAN_FRONTEND=noninteractive apt -y install mysql-community-server
+
+#apt -y install mysql-server
 
 #apt install git -y
 #git clone https://github.com/ISarrz/zelenka_server.git
