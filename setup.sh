a71150e54d25f8930282603f09bbc0457531e577
ISarrz <germanzlobin2@gmail.com>
2026-03-10 19:01:15 +0000

    фыва

--- setup.sh
+++ setup.sh
@@ -7,10 +7,14 @@ echo
 
 apt-get update -y
 apt-get upgrade -y
+apt-get install -y wget gnupg git tmux curl
 
-apt-get install -y wget gnupg git tmux docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 
+systemctl start docker
+systemctl enable docker
 
 git clone "https://${GITHUB_TOKEN}@github.com/ISarrz/zelenka_server.git"
 
-echo "Репозиторий успешно клонирован!"
+echo "Установка завершена! Docker готов к работе."
